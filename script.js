adffeed9bcefb4b4cc6b3a60a9444ecfb7556e33
kiên lê <lexuankien122@gmail.com>
2023-12-13 21:34:35 +0700

    fix UI of login

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('signUp').addEventListener('click', function() {
-    document.querySelector('.container').classList.add('right-panel-active');
-  });
-  
-  document.getElementById('signIn').addEventListener('click', function() {
-    document.querySelector('.container').classList.remove('right-panel-active');
-  });
-  
